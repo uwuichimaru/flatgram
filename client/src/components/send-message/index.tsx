e25f6cb1f2bb0c3e566321fbacfd0b3d00419f29
Islam-77 <islammirzabekov228@gmail.com>
2024-01-03 12:53:27 +0300

    send message panel 2/2

--- client/src/components/send-message/index.tsx
+++ client/src/components/send-message/index.tsx
@@ -1,19 +1,34 @@
-import styles from './SendMessage.module.scss'
-import sendMessage from '../../assets/send-message/SendMessage.svg'
-import link from '../../assets/send-message/Link.svg'
-import EmojiPicker from 'emoji-picker-react'
-import Smiley from '../../assets/send-message/Smiley.svg'
+import styles from "./SendMessage.module.scss";
+import sendMessage from "../../assets/send-message/SendMessage.svg";
+import link from "../../assets/send-message/Link.svg";
+import { useState } from "react";
+import InputEmoji from "react-input-emoji";
+ 
 
 export const SendMessage = () => {
-    return (
-        <div className={styles.message}>
+ 
 
-            <img src={link} alt="" className={styles.message__link} />
-            <input type="text" placeholder='Write a message ...' />
-            <img src={Smiley} alt="" className={styles.message__smiley}/>
-            <button>
-                <img src={sendMessage} alt="" />
-            </button>
-        </div>
-    )
-}
+  const [text, setText] = useState<string>("");
+  function handleOnEnter(text: string) {
+    console.log("enter", text);
+  }
+ 
+  return (
+    <div className={styles.message}>
+      <img src={link} alt="" className={styles.message__link} />
+      {/* <img src={Smiley} alt="" className={styles.message__smiley} /> */}
+ 
+      <InputEmoji
+      inputClass={styles.message__input}
+      value={text}
+      onChange={setText}
+      onEnter={handleOnEnter}
+      placeholder="Write a message ..."
+      keepOpened={true}
+    />
+      <span className={styles.message__button}>
+        <img src={sendMessage} alt="" />
+      </span>
+    </div>
+  );
+};
